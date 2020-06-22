02bd6f05c5a738dbf68423220570fb60b5278ada
Minh Huy <859@hobertonschool.com>
2020-06-22 04:21:14 -0400

    tetronimo collision, line clear

--- Assets/Tetronimo.cs
+++ Assets/Tetronimo.cs
@@ -11,6 +11,8 @@ public class Tetronimo : MonoBehaviour
     public static int height = 20;
     public static int width = 10;
 
+    private static Transform[,] grid = new Transform[width, height];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -46,10 +48,70 @@ public class Tetronimo : MonoBehaviour
             if(!ValidMove())
             {
                 transform.position += new Vector3(0, 1, 0);
+                AddToGrid();
+                CheckForLines();
                 this.enabled = false;
                 FindObjectOfType<spawnTetronimo>().NewTetronimo();
             }
             prevTime = Time.time;
+        }
+    }
+
+    void CheckForLines()
+    {
+        for (int a = height - 1; a >= 0; a--)
+        {
+            if (HasLine(a))
+            {
+                DeleteLine(a);
+                RowDown(a);
+            }
+        }
+    }
+
+    bool HasLine(int a)
+    {
+        for (int b = 0; b < width; b++)
+        {
+            if (grid[b, a] == null)
+                return false;
+        }
+        return true;
+    }
+
+    void DeleteLine(int a)
+    {
+        for (int b = 0; b < width; b++)
+        {
+            Destroy(grid[b,a].gameObject);
+            grid[b,a] = null;
+        }
+    }
+
+    void RowDown(int a)
+    {
+        for (int c = a; c < height; c++)
+        {
+            for (int d = 0; d < width; d++)
+            {
+                if (grid[d,c] != null)
+                {
+                    grid[d, c -1] = grid[d, c];
+                    grid[d, c] = null;
+                    grid[d, c - 1].transform.position -= new Vector3(0, 1, 0);
+                }
+            }
+        }
+    }
+
+    void AddToGrid()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            grid[roundX, roundY] = children;
         }
     }
 
@@ -64,6 +126,9 @@ public class Tetronimo : MonoBehaviour
             {
                 return false;
             }
+
+            if(grid[roundX,roundY] != null)
+                return false;
         }
         return true;
     }
