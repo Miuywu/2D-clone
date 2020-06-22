fdcf8261a7c33b3b91d253dcfb96bc5bb601c792
Minh Huy <859@hobertonschool.com>
2020-06-22 03:14:13 -0400

    new teronimos, random spawn, falling, board

--- Assets/Tetronimo.cs
+++ Assets/Tetronimo.cs
@@ -4,8 +4,12 @@ using UnityEngine;
 
 public class Tetronimo : MonoBehaviour
 {
+    public Vector3 rotationPoint;
+
     private float prevTime;
     public float fallTime = 0.8f;
+    public static int height = 20;
+    public static int width = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -19,16 +23,48 @@ public class Tetronimo : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += new Vector3(-1, 0, 0);
+            if(!ValidMove())
+                transform.position += new Vector3(1, 0, 0);
         }
         else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += new Vector3(1, 0, 0);
+            if(!ValidMove())
+                transform.position += new Vector3(-1, 0, 0);
+        }
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), 90);
+            if(!ValidMove())
+                transform.RotateAround(transform.TransformPoint(rotationPoint), new Vector3(0,0,1), -90);
         }
 
         if (Time.time - prevTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
         {
             transform.position += new Vector3(0, -1, 0);
+            if(!ValidMove())
+            {
+                transform.position += new Vector3(0, 1, 0);
+                this.enabled = false;
+                FindObjectOfType<spawnTetronimo>().NewTetronimo();
+            }
             prevTime = Time.time;
         }
+    }
+
+    bool ValidMove()
+    {
+        foreach (Transform children in transform)
+        {
+            int roundX = Mathf.RoundToInt(children.transform.position.x);
+            int roundY = Mathf.RoundToInt(children.transform.position.y);
+
+            if(roundX < 0 || roundX >= width || roundY < 0 || roundY >= height)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
---
+++ Assets/spawnTetronimo.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnTetronimo : MonoBehaviour
+{
+    public GameObject[] Tetronimoes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NewTetronimo();
+    }
+
+    public void NewTetronimo()
+    {
+        Instantiate(Tetronimoes[Random.Range(0, Tetronimoes.Length)], transform.position, Quaternion.identity);
+    }
+}
