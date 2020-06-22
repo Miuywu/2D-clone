2670705ec5520425784aef67274c8730044b49e1
Minh Huy <859@hobertonschool.com>
2020-06-22 02:18:32 -0400

    first commit

---
+++ Assets/Tetronimo.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tetronimo : MonoBehaviour
+{
+    private float prevTime;
+    public float fallTime = 0.8f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            transform.position += new Vector3(-1, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            transform.position += new Vector3(1, 0, 0);
+        }
+
+        if (Time.time - prevTime > (Input.GetKey(KeyCode.DownArrow) ? fallTime / 10 : fallTime))
+        {
+            transform.position += new Vector3(0, -1, 0);
+            prevTime = Time.time;
+        }
+    }
+}
